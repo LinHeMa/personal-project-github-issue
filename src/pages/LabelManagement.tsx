be37e3ed3ddd9c976f75e71c4af4877d5c88076e
linHeMa <linhema0921@gmail.com>
2022-09-20 10:22:32 +0800

    start to layout header

---
+++ src/pages/LabelManagement.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const LabelManagement = () => {
+  return (
+    <div>LabelManagement</div>
+  )
+}
+
+// export default LabelManagement 
