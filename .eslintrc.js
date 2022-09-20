b1f4e00a8bc465fa76ae7fa48ee03695c2778a5a
linHeMa <linhema0921@gmail.com>
2022-09-20 09:57:53 +0800

    add .env file to store token

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 /* eslint-env node */
 module.exports = {
   env: {
+    node: true,
     browser: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
--- src/supabase/client.js
+++ src/supabase/client.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   'https://dleytiekgsgorradcnjr.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsZXl0aWVrZ3Nnb3JyYWRjbmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM1NzkwODEsImV4cCI6MTk3OTE1NTA4MX0.pgLsr0LZlMfQ7dC_iyK-mFjsCC4UxTibN2Vvu2OlV8U'
+  process.env.REACT_APP_SUPABASE_TOKEN
 );
 
 export { supabase };
