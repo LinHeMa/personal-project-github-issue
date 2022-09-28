e3e7ec41165a513c34d236b06214b653bb94d7fa
linHeMa <linhema0921@gmail.com>
2022-09-28 09:16:07 +0800

    done popup start to do map and onClick

--- src/components/IssuePage/IssueListItem.tsx
+++ src/components/IssuePage/IssueListItem.tsx
@@ -1,77 +1,80 @@
 import {
   CommentIcon,
-  CommitIcon,
-  IssueClosedIcon,
   IssueOpenedIcon,
-  TriangleDownIcon,
-} from "@primer/octicons-react";
-import React from "react";
-import Label from "../label/Label";
-import avatar from "../../images/github_avatar.png";
-import PopupMenu from "./PopupMenu";
+  TriangleDownIcon
+} from '@primer/octicons-react';
+import React, { useState } from 'react';
+import Label from '../label/Label';
+import avatar from '../../images/github_avatar.png';
+import PopupMenu from './PopupMenu';
 
-const subtitleList: string[] = ["Label", "Assignee", "Sort"];
+const subtitleList: string[] = ['Label', 'Assignee', 'Sort'];
 
 const IssueListItem = () => {
+  const [labelPop, setLabelPop] = useState(false);
+  console.log(labelPop);
   return (
-    <div className="container  md:px-6 lg:px-8 mx-auto mt-4 ">
-      <div className="flex items-center  justify-between px-4 bg-primary-bg-gray border-solid border border-stone-300 sm:rounded-t-lg p-7 md:justify-start">
-        <input type="checkbox" className="mr-4 hidden sm:block" />
+    <div className='container  md:px-6 lg:px-8 mx-auto mt-4 '>
+      <div className='flex items-center  justify-between px-4 bg-primary-bg-gray border-solid border border-stone-300 sm:rounded-t-lg p-7 md:justify-start'>
+        <input type='checkbox' className='mr-4 hidden sm:block' />
         {subtitleList.map((item, index) => (
           <div
+            onClick={() => {
+              setLabelPop(true);
+            }}
             key={index}
-            className="cursor-pointer h-[21px] flex items-center px-4 text-primary-icon-gray text-[14px]"
+            className='cursor-pointer h-[21px] flex items-center px-4 text-primary-icon-gray text-[14px]'
           >
             {item}
-            <a href="#" className="hidden sm:block">
-              <TriangleDownIcon fill="#57606A" />
+            <a href='#' className='hidden sm:block'>
+              <TriangleDownIcon fill='#57606A' />
             </a>
+            <PopupMenu labelpop={labelPop} setLabelPop={setLabelPop} />
           </div>
         ))}
       </div>
-      <div className="flex h-[85px] md:h-[62.31px] border-b-[1px] sm:border sm:border-t-[0px] border-solid  border-stone-300 last:rounded-b-lg">
-        <div className="flex justify-center items-start pl-[10px] py-[12.5px]">
-          <input type="checkbox" className="mr-4 hidden sm:block" />
+      <div className='flex h-[85px] md:h-[62.31px] border-b-[1px] sm:border sm:border-t-[0px] border-solid  border-stone-300 last:rounded-b-lg'>
+        <div className='flex justify-center items-start pl-[10px] py-[12.5px]'>
+          <input type='checkbox' className='mr-4 hidden sm:block' />
         </div>
-        <div className="pl-4 pt-5  self-start">
-          <IssueOpenedIcon fill="#1a7f37" />
+        <div className='pl-4 pt-5  self-start'>
+          <IssueOpenedIcon fill='#1a7f37' />
         </div>
-        <div className="p-5 pl-3 pr-4 flex  flex-col items-start md:flex-row md:flex-wrap md:justify-start  ">
-          <h1 className="font-semibold text-[16px] leading-5">很多bug喔</h1>
-          <div className="flex mt-3 flex-col  items-start leading-5 md:mt-0 md:ml-2 ">
+        <div className='p-5 pl-3 pr-4 flex  flex-col items-start md:flex-row md:flex-wrap md:justify-start  '>
+          <h1 className='font-semibold text-[16px] leading-5'>很多bug喔</h1>
+          <div className='flex mt-3 flex-col  items-start leading-5 md:mt-0 md:ml-2 '>
             <Label
-              text="Label"
-              bgColor="#eeeccc"
-              borderColor="transparent"
-              fontWeight="700"
+              text='Label'
+              bgColor='#eeeccc'
+              borderColor='transparent'
+              fontWeight='700'
             />
           </div>
-          <a className="whitespace-pre mt-3 text-primary-icon-gray md:w-full text-start">
+          <a className='whitespace-pre mt-3 text-primary-icon-gray md:w-full text-start'>
             #id opened 10 days ago by LinHeMa
           </a>
         </div>
-        <div className="hidden sm:flex ml-auto  items-start pt-5 pr-4 ">
-          <div className="flex group">
+        <div className='hidden sm:flex ml-auto  items-start pt-5 pr-4 '>
+          <div className='flex group'>
             <img
               src={avatar}
-              className="h-[20px] w-[20px] rounded-full mr-[-12px] group-hover:mr-2"
+              className='h-[20px] w-[20px] rounded-full mr-[-12px] group-hover:mr-2'
             />
             <img
               src={avatar}
-              className="h-[20px] w-[20px] rounded-full mr-[-12px] group-hover:mr-2 "
+              className='h-[20px] w-[20px] rounded-full mr-[-12px] group-hover:mr-2 '
             />
             <img
               src={avatar}
-              className="h-[20px] w-[20px] rounded-full mr-8  md:mr-[40px]"
+              className='h-[20px] w-[20px] rounded-full mr-8  md:mr-[40px]'
             />
           </div>
-          <div className="flex items-center">
-            <CommentIcon fill="#57606A" />
-            <p className="pl-2">3</p>
+          <div className='flex items-center'>
+            <CommentIcon fill='#57606A' />
+            <p className='pl-2'>3</p>
           </div>
         </div>
       </div>
-      <PopupMenu />
     </div>
   );
 };
--- src/components/IssuePage/PopupMenu.tsx
+++ src/components/IssuePage/PopupMenu.tsx
@@ -1,43 +1,67 @@
 import { CheckIcon } from "@primer/octicons-react";
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
-const PopupMenu = () => {
+type PopupMenuProps = {
+  labelpop: boolean;
+  setLabelPop: Dispatch<SetStateAction<boolean>>;
+};
+
+const PopupMenu = ({ labelpop, setLabelPop }: PopupMenuProps) => {
   return (
     <>
-      <div className="bg-black opacity-40 fixed w-full h-full top-0 left-0 z-10" />
-      <div className="bg-white opacity-100 absolute z-20 top-[3%] right-[20px] left-[20px] flex flex-col  mx-auto border border-solid border-primary-icon-gray rounded-lg">
-        <div className="flex justify-between items-center h-[54px] p-[16px] border-b-[1px] border-solid border-stone-300">
-          <div className="font-semibold text-[14px] ">Filter by label</div>
-          <div className="text-primary-icon-gray">X</div>
-        </div>
-        <div className="font-semibold text-[14px] p-[16px] border-solid border-b-[1px] border-stone-300">
-          <input
-            type="text"
-            className="w-full px-[12px] py-[5px] h-[32px] border border-solid border-stone-300 rounded-lg font-medium"
-            placeholder="Filter labels"
+      {labelpop && (
+        <div>
+          <div
+            onClick={(e) => {
+              console.log("clicked");
+              setLabelPop(false);
+              e.stopPropagation();
+            }}
+            className="bg-black opacity-40 fixed w-full h-full top-0 left-0 z-10"
           />
-        </div>
-        <div className="font-semibold text-[14px] p-[16px] leading-[21px] flex justify-start items-center">
-          <div className=" mr-3">
-            <CheckIcon />
+          <div className="group-last:rounded-b-lg bg-white opacity-100 absolute z-20 top-[3%] right-[20px] left-[20px] flex flex-col  mx-auto border border-solid border-primary-icon-gray rounded-lg">
+            <div className="flex justify-between items-center h-[54px] p-[16px] border-b-[1px] border-solid border-stone-300">
+              <div className="font-semibold text-[14px] ">Filter by label</div>
+              <div
+                onClick={(e) => {
+                  setLabelPop(false);
+                  e.stopPropagation();
+                }}
+                className="text-primary-icon-gray"
+              >
+                X
+              </div>
+            </div>
+            <div className="font-semibold text-[14px] p-[16px] border-solid border-b-[1px] border-stone-300">
+              <input
+                type="text"
+                className="w-full px-[12px] py-[5px] h-[32px] border border-solid border-stone-300 rounded-lg font-medium"
+                placeholder="Filter labels"
+              />
+            </div>
+            <div className="font-semibold text-[14px] p-[16px] leading-[21px] flex justify-start items-center">
+              <div className=" mr-3">
+                <CheckIcon />
+              </div>
+              Unlabeled
+            </div>
+            <div className="font-semibold text-[14px] p-[16px] leading-[21px] flex justify-start items-center">
+              <div className=" mr-3">
+                <CheckIcon />
+              </div>
+              <div className=" mr-3 w-[14px] h-[14px] rounded-full bg-red-300" />
+              Apple
+            </div>
+            <div className="font-semibold text-[14px] p-[16px] leading-[21px] flex justify-start items-center">
+              <div className=" mr-3">
+                <CheckIcon />
+              </div>
+              <div className=" mr-3 w-[14px] h-[14px] rounded-full bg-red-300" />
+              Banana
+            </div>
           </div>
-          Unlabeled
         </div>
-        <div className="font-semibold text-[14px] p-[16px] leading-[21px] flex justify-start items-center">
-          <div className=" mr-3">
-            <CheckIcon />
-          </div>
-          <div className=" mr-3 w-[14px] h-[14px] rounded-full bg-red-300" />
-          Apple
-        </div>
-        <div className="font-semibold text-[14px] p-[16px] leading-[21px] flex justify-start items-center">
-          <div className=" mr-3">
-            <CheckIcon />
-          </div>
-          <div className=" mr-3 w-[14px] h-[14px] rounded-full bg-red-300" />
-          Banana
-        </div>
-      </div>
+      )}
     </>
   );
 };
